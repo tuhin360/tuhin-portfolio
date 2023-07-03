4127a2c203837adebdeda4c51824389951e80587
tuhin <tuhin153695@gmail.com>
2023-07-03 18:18:06 +0600

    social link and resume added

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -1,11 +1,80 @@
-import React from 'react';
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const SocialLinks = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn <FaLinkedin size={30}></FaLinkedin>
+        </>
+      ),
+      href: "https://linkedin.com",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub <FaGithub size={30}></FaGithub>
+        </>
+      ),
+      href: "https://github.com/tuhin360",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={30}></HiOutlineMail>
+        </>
+      ),
+      href: "mailto:tuhin153695@gmail.com",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={30}></BsFillPersonLinesFill>
+        </>
+      ),
+      href: "/resume.pdf",
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
+
+  return (
+    <div className="hidden lg:flex flex-col top-[40%] left-0 fixed">
+      <ul>
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 " +
+              style
+            }
+            style={{
+              transitionProperty: "margin-left", // Define the property to transition
+              transitionDuration: "0.3s", // Specify the duration of the transition
+            }}
+          > 
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default SocialLinks;
