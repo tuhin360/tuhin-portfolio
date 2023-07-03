085a11072bcab6b3578e3e7377677c237cc88273
tuhin <tuhin153695@gmail.com>
2023-07-03 17:06:35 +0600

    home banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Home from './components/Home/Home'
 import NavBar from './components/Navbar/NavBar'
+import SocialLinks from './components/SocialLinks/SocialLinks'
 
 function App() {
   return (
      <div>
       <NavBar></NavBar>
       <Home></Home>
+      <SocialLinks></SocialLinks>
      </div>
   )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import firstImg from "../../assets/portfolio/profile01.png";
-import { FaArrowRight } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div name="home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800 ">
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 ">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Front End Web Developer</h2>
-          <p className="text-gray-500 py-4">
+          <p className="text-gray-500 py-4 max-w-md">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
             error nisi voluptate, quis eos accusamus laudantium distinctio
             incidunt ullam mollitia sed tempore ea ratione voluptas natus eaque
             necessitatibus unde officiis
           </p>
           <div>
-            <button>
-              Portfolio <span><FaArrowRight /></span>
+            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer font-b">
+              Portfolio <span className="group-hover:rotate-90 duration-300 ml-1"><FaChevronRight size={15}/></span>
             </button>
           </div>
         </div>
